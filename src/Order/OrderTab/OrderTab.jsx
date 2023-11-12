b1dfea7b1342b553be5a9975eb073f7bec19b8e9
Marufg <marufb572@gmail.com>
2023-11-12 20:31:08 +0600

    added login and logout pages

--- src/Order/OrderTab/OrderTab.jsx
+++ src/Order/OrderTab/OrderTab.jsx
@@ -22,7 +22,7 @@ const OrderTab = ({ items }) => {
             >
                 <SwiperSlide>
                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-                   {
+                    {
                         items.map(item => <FoodCard key={item._id} item={item}></FoodCard>)
                     }
                    </div>
