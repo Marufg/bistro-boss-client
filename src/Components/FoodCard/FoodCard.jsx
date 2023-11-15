48f1039383d76cdb439af4e6a1975ead639d2cc8
Marufg <marufb572@gmail.com>
2023-11-15 15:01:00 +0600

    added cart item section

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -5,20 +5,21 @@ import Swal from "sweetalert2";
 import { useLocation, useNavigate } from "react-router-dom";
 
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useCart from "../../hooks/useCart";
 
 
 const FoodCard = ({ item }) => {
   const { name, image, price, recipe, _id } = item;
-  const { user } = useAuth()
-  const navigate = useNavigate()
-  const location = useLocation()
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const axiosSecure = useAxiosSecure();
+  const [, refetch] = useCart();
 
-  const axiosSecure = useAxiosSecure()
-
-  const handleAddToCart = food => {
+  const handleAddToCart = () => {
     if (user && user.email) {
       // todo
-      console.log(user.email, food);
+    
       const cartItem = {
         menuId: _id,
         email: user.email,
@@ -28,21 +29,22 @@ const FoodCard = ({ item }) => {
 
       }
       axiosSecure.post('/carts', cartItem)
-      .then(res => {
-        console.log(res.data);
-        if (res.data.insertedId) {
-          Swal.fire({
-            position: "top-end",
-            icon: "success",
-            title: `${name} added to your cart`,
-            showConfirmButton: false,
-            timer: 1500
-          });
-        }
-      })
+        .then(res => {
+          console.log(res.data);
+          if (res.data.insertedId) {
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: `${name} added to your cart`,
+              showConfirmButton: false,
+              timer: 1500
+            });
+            refetch()
+          }
+        })
     }
 
-  
+
     else {
       Swal.fire({
         title: "You are not logged in",
@@ -69,7 +71,7 @@ const FoodCard = ({ item }) => {
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-end">
-          <button onClick={() => handleAddToCart(item)} className="btn btn-outline bg-slate-100 border-orange-400 border-0 border-b-4 mt-4">Add To Cart</button>
+          <button onClick={handleAddToCart} className="btn btn-outline bg-slate-100 border-orange-400 border-0 border-b-4 mt-4">Add To Cart</button>
         </div>
       </div>
     </div>
